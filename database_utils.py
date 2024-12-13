002da0d16957d5d5a9d47d4c76b77227c36427e6
Mimran0715 <maleeha.imranm@gmail.com>
2024-12-13 13:08:14 -0800

    starting dev

---
+++ database_utils.py
@@ -0,0 +1,12 @@
+import sqlite3
+import os
+
+
+def create_table():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ text_collection.py
@@ -0,0 +1,17 @@
+import requests
+import json
+import os
+
+API_TOKEN = ""
+
+def build_url():
+    pass
+
+def parse_response():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
